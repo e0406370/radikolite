afd891599312c2ff412c921093a38c3412a669ef
e0406370 <e0406370@u.nus.edu>
2024-12-02 22:42:57 +0800

    feat: retrieve and render radio data from json file

--- radikolite_home/models.py
+++ radikolite_home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Radio(models.Model):
+    radio_name = models.CharField(max_length=64)
+    radio_logo = models.URLField(max_length=128, blank=True, null=True)
+    radio_stream = models.URLField(max_length=128)
+
+    def __str__(self):
+        return f"Radio: {self.radio_name}, Logo: {self.radio_logo}, Stream: {self.radio_stream}"
